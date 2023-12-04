dfdb74e0d0993f611cffa2cf4e450792ab1a2643
wesmoni <isilinkina25565137@gmail.com>
2023-12-05 00:04:17 +0500

    Добавлено редактирование предметов

--- src/main/java/com/example/spring2023/controllers/MainController.java
+++ src/main/java/com/example/spring2023/controllers/MainController.java
@@ -61,6 +61,30 @@ public class MainController {
         return "items-info";
     }
 
+    @GetMapping("/items/{id}/edit")
+    public String itemsEdit(@PathVariable(value = "id") long id, Model model) {
+        Optional<Items> item = itemsRepository.findById(id);
+        ArrayList<Items> result = new ArrayList<>();
+        item.ifPresent(result::add);
+        model.addAttribute("item", result);
+        return "items-edit";
+    }
+
+    @PostMapping("/items/{id}/edit")
+    public String itemsUpdate(@PathVariable(value = "id") long id, @RequestParam String name, @RequestParam String rarity, @RequestParam String description, @RequestParam String type, @RequestParam String source, Model model) {
+        Optional<Items> item = itemsRepository.findById(id);
+        if (item.isPresent()) {
+            Items updatedItem = item.get();
+            updatedItem.setName(name);
+            updatedItem.setRarity(rarity);
+            updatedItem.setDescription(description);
+            updatedItem.setType(type);
+            updatedItem.setSource(source);
+            itemsRepository.save(updatedItem);
+        }
+        return "redirect:/items";
+    }
+
     @PostMapping("/items/add")
     public String itemsPostAdd(@RequestParam String name, @RequestParam String rarity, @RequestParam String description, @RequestParam String type, @RequestParam String source, Model model) {
         Items items = new Items(name, rarity, description, type, source);
